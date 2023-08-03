03c6c7ca4b74a0fd5ee936f2abe4aee5b2ea0cd9
xenonth <timothy.forbes259@gmail.com>
2023-08-03 11:29:06 +0930

    created first python script

---
+++ sum.py
@@ -0,0 +1,11 @@
+# input the first number an integer
+number1 = int(input("Enter your first number here: "))
+
+# input the second number an integer
+number2 = int(input("Enter your second number here: "))
+
+# processing
+sum_of_numbers = number1 + number2
+
+# output
+print("The sum is ", sum_of_numbers)
